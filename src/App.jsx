782b59de3830c70d2fa63c5f38a1fef001e1991c
maheksinghal04 <mahek1910188@akgec.ac.in>
2022-05-03 14:37:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { Watch } from "@material-ui/icons";
+import "./app.scss"
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch"
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+const App = () => {
+  return <div>
+  {/* <Home/> */}
+    {/* <Register/> */}
+    {/* <Login/> */}
+    <Watch/>
+  </div>;
+};
+
+export default App;
